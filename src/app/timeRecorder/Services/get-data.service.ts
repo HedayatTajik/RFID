36321779ea4dd655b3d946cddefe7d20afb52dcf
Hedayatullah Tajik <hedayatullah.tajik2@sma.de>
2022-07-19 10:58:14 +0200

    feat: add neue table form

--- src/app/timeRecorder/Services/get-data.service.ts
+++ src/app/timeRecorder/Services/get-data.service.ts
@@ -1,17 +1,33 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/compat/firestore';
+import {
+  AngularFireDatabase,
+  AngularFireList,
+  AngularFireObject,
+} from '@angular/fire/compat/database';
 import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class GetDataService {
+  studentsRef!: AngularFireList<any>;
+  studentRef!: AngularFireObject<any>;
 
   constructor(
-    private firestore: AngularFirestore
+    private firestore: AngularFirestore,
+    
   ) { }
 
+
   getData(): Observable<any> {
-    return this.firestore.collection('TimeRecorder').valueChanges();
+
+    // const shirtsCollection = this.firestore.collection<any>('tshirts');
+    // shirtsCollection.add({ name: 'item', price: 10 });
+    return this.firestore.collection('UsersEntity').valueChanges();
   }
 
 }
+
+
+
+//https://www.youtube.com/watch?v=IoVOFdSZqzk
