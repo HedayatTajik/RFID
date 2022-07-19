25ceba3350b2d3c578c49ea7f86d7cde976c37fd
Hedayatullah Tajik <hedayatullah.tajik2@sma.de>
2022-07-19 17:04:38 +0200

    feat: add about

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './timeRecorder/Components/about/about.component';
 import { DashboardComponent } from './timeRecorder/Components/dashboard/dashboard.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'about', component: AboutComponent },
 ];
 
 @NgModule({
--- src/app/timeRecorder/Components/about/about.component.ts
+++ src/app/timeRecorder/Components/about/about.component.ts
@@ -1,12 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
-  pdfSrc = "src/assets/S108_Einrichtung_der_Hardware.pdf"
+  src: string = '../../../../assets/S108_Einrichtung_der_Hardware.pdf';
   constructor() { }
 
   ngOnInit(): void {
