3712af3d1349804f604f220f11fddda02ba6365a
Hedayatullah Tajik <hedayatullah.tajik2@sma.de>
2022-08-25 14:01:26 +0200

    feat: removed not nedded codes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,33 +10,6 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-
-    var timestamp = Date.now()
-    var timestamp2 = Date.now() - 120000
-    console.log("timestamp1:", timestamp);
-
-
-    const date = new Date(timestamp);
-    console.log("date:",date);
-
-    const year = date.getFullYear();
-    console.log("year",year); 
-
-    const month = date.getMonth() +1;
-    console.log("Month",month); 
-    
-
-    const day = date.getDate();
-    console.log("day",day); 
-
-    const time = date.getTime();
-    console.log("time",time); 
-
-
-   var  human = new Date(timestamp).toLocaleString() 
-   var  human2 = new Date(timestamp2).toLocaleString()
-    console.log("human:",human );
-    console.log("human2:",human2 );
   }
 
 }
